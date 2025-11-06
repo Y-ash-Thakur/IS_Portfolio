ca81feaffc9b979952528cec3746d5ea27aff09c
Y-ash-Thakur <yasht0509@gmail.com>
2025-11-06 20:12:19 +0530

    testimonial section improved

--- data/index.ts
+++ data/index.ts
@@ -129,12 +129,6 @@ export const testimonials = [
     name: "Mr. Shivam Singh",
     title: "Owner - Trail Blaze",
   },
-  {
-    quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
-  },
 ];
 
 export const companies = [
